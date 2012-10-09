d89d836547cff54ebbb2b4a99cc9a6a147802d29
Quach <quachkation@gmail.com>
2012-10-09 21:13:49 +0200

    1 modify

--- map.py
+++ map.py
@@ -21,22 +21,27 @@ def map_yield(func, iterator):
 
 def map_rq_yield(func, iterator):
     "map rereregenerator"
+    
+    yield func(iterator[0])
 
-    pass
+    for x in map_rq_yield(func, iterator[1: ]):
+        yield x
 
-def time_me(func, dict):
+def time_me(func, stat_dict):
     "function with params"
+    stat_dict.setdefault('num_calls', 0)
+    stat_dict.setdefault('cum_time', 0)
     def decorator(main_func):
         "decorator"
+
         def wrapper(*args, **kwargs):
             "rapper function to profilling func"
-            dict['num_calls'] = (dict['num_calls'] + 1 
-                                 if 'num_calls' in dict else 1)
+
+            stat_dict['num_calls'] += 1
             time_1 = func()
             res = main_func(*args, **kwargs)
             time_2 = func()
-            dict['cum_time'] = (dict['cum_time'] + time_2 - time_1 
-                                if 'cum_time' in dict else time_2 - time_1)
+            stat_dict['cum_time'] += time_2 - time_1
             return res
         return wrapper
     return decorator
@@ -52,6 +57,12 @@ def main():
     assert next(gen) == 4
     assert next(gen) == 9
     print "Map generator OK!"
+
+    gen1 = map_rq_yield(lambda x : x ** 2, [1, 2, 3])
+    assert next(gen1) == 1
+    assert next(gen1) == 4
+    assert next(gen1) == 9
+    print "Map recurse generator OK!"
 
     statistics = {}
     @time_me(time.clock, statistics)
